db462c72c68a5b0bdf65a5170eced9e1b3353b0b
cpeinado <cpeinadoggmail.com>
2014-06-05 20:14:08 +0200

    Filter by Rating

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,14 @@ class MoviesController < ApplicationController
   def index
     @movies = Movie.all
 
+    if (@checked != nil)
+	@movies = @movies.find_all{|m| @checked.has_key?(m.rating) and @checked[m.rating] == true}
+    end
+
+    if(params[:ratings] != nil)
+       @movies = @movies.find_all{ |m| params[:ratings].has_key?(m.rating) }
+    end
+
     if(params[:sort].to_s == 'title')
 	@movies = @movies.sort_by {|m| m.title}
     end
@@ -16,6 +24,21 @@ class MoviesController < ApplicationController
     if(params[:sort].to_s == 'date')
 	@movies = @movies.sort_by {|m| m.release_date}
     end 
+
+
+    @all_ratings = ['G','PG','PG-13','R']
+
+    @checked = {}
+
+    @all_ratings.each { |rating|
+
+   	if params[:ratings] == nil
+        	@checked[rating] = false
+   	else
+        	@checked[rating] = params[:ratings].has_key?(rating)
+   	end
+     }
+
 
   end
 
